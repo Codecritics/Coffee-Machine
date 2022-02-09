471137f2425ad89a0486d1e7beb234389749e520
Tanawa Tsamo Marius <mariustanawa@gmail.com>
2022-02-09 11:05:44 +0100

    Stage 1/6

--- machine/coffee_machine.py
+++ machine/coffee_machine.py
@@ -1,1 +1,9 @@
-# Write your code here
+msg_0 = "Starting to make a coffee"
+msg_1 = "Grinding coffee beans"
+msg_2 = "Boiling water"
+msg_3 = "Mixing boiled water with crushed coffee beans"
+msg_4 = "Pouring coffee into the cup"
+msg_5 = "Pouring some milk into the cup"
+msg_6 = "Coffee is ready!"
+
+print(msg_0, msg_1, msg_2, msg_3, msg_4, msg_5, msg_6, sep="\n")
